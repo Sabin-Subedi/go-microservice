490835f8cac443d942122c9546fd80d88e520739
Sabin Subedi <sabinsubedi125@gmail.com>
2023-11-06 09:40:22 +0545

    feat: add the helper functions

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -18,11 +10,6 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker endpoint",
 		Data:    nil,
 	}
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	_, err := w.Write(out)
-	if err != nil {
-		log.Println(err)
-	}
+	_ = app.WriteJSON(w, http.StatusOK, payload)
 }
+
